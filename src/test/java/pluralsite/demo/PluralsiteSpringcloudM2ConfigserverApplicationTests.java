cca0ffed30de0ebca417895dda012b759202b959
Goodnews <goodnews@Goodnewss-MacBook-Pro.local>
2021-05-02 09:38:11 +0100

    First upload

---
+++ src/test/java/pluralsite/demo/PluralsiteSpringcloudM2ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package pluralsite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsiteSpringcloudM2ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
